40528b1f251390841e7c71ade0f514451c488ce7
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-03-27 22:28:38 +0300

    initial

---
+++ aliases.sh
@@ -0,0 +1,21 @@
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias md='mkdir -p'
+
+alias h='history'
+alias j='jobs -l'
+alias which='type -a'
+alias path='echo -e ${PATH//:/\\n}'
+alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
+alias more=less
+
+alias du='du -kh'       # Makes a more readable output.
+alias df='df -kTh'
+
+alias psu='ps -o pid,rss,command -w'
+
+alias l="ls -alFG"
+
+
---
+++ functions.sh
@@ -0,0 +1,53 @@
+#
+# This extenstion sets bash prompt to include 
+#
+function __define_colors
+{
+    # Configure Colors:
+    WHITE='\[\033[1;37m\]'
+    LIGHTGRAY='\[033[0;37m\]'
+    GRAY='\[\033[1;30m\]'
+    BLACK='\[\033[0;30m\]'
+    RED='\[\033[0;31m\]'
+    LIGHTRED='\[\033[1;31m\]'
+    GREEN='\[\033[0;32m\]'
+    LIGHTGREEN='\[\033[1;32m\]'
+    BROWN='\[\033[0;33m\]'
+    YELLOW='\[\033[1;33m\]'
+    BLUE='\[\033[0;34m\]'
+    LIGHTBLUE='\[\033[1;34m\]'
+    PURPLE='\[\033[0;35m\]'
+    PINK='\[\033[1;35m\]'
+    CYAN='\[\033[0;36m\]'
+    LIGHTCYAN='\[\033[1;36m\]'
+    DEFAULT='\[\033[0m\]'
+}
+
+function __set_prompt
+{
+    export PS1="${GREEN}\u.\h${LIGHTBLUE}"' $(__git_ps1 "(%s) ")'"${BROWN}\w ${DEFAULT}\\$ "
+}
+
+function extract()      # Handy Extract Program.
+{
+    if [ -f $1 ] ; then
+	case $1 in
+	    *.tar.bz2)   tar xvjf $1     ;;
+	    *.tar.gz)    tar xvzf $1     ;;
+	    *.bz2)       bunzip2 $1      ;;
+	    *.rar)       unrar x $1      ;;
+	    *.gz)        gunzip $1       ;;
+	    *.tar)       tar xvf $1      ;;
+	    *.tbz2)      tar xvjf $1     ;;
+	    *.tgz)       tar xvzf $1     ;;
+	    *.zip)       unzip $1        ;;
+	    *.Z)         uncompress $1   ;;
+	    *.7z)        7z x $1         ;;
+	    *)           echo "'$1' cannot be extracted via >extract<" ;;
+	 esac
+    else
+	echo "'$1' is not a valid file"
+    fi
+}
+
+
