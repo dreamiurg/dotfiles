2b03050b01128b185fc60da997c87013ec0d9d84
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-06-01 14:50:49 -0700

    Edited misc.sh via GitHub

--- misc.sh
+++ misc.sh
@@ -1,2 +1,7 @@
 # Globally exclude some files
 git config --global core.excludesfile ~/etc/bash-ext/.gitignore_global
+git config --global alias.st status
+git config --global alias.last "log -1 HEAD"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
