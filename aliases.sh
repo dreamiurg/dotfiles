ad1d024c7e9c414b17053f087d8af347ea377cb5
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-09-29 13:59:30 +0300

    Added support for `git diffall`

--- aliases.sh
+++ aliases.sh
@@ -1,23 +1,25 @@
+# Filesystem
+alias l='ls -alFG'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-
 alias md='mkdir -p'
+alias du='du -kh'       # Makes a more readable output.
+alias df='df -kTh'
 
+# Other
 alias h='history'
 alias j='jobs -l'
 alias which='type -a'
 alias path='echo -e ${PATH//:/\\n}'
 alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
+alias psu='ps -o pid,rss,command -w'
 alias more=less
 
-alias du='du -kh'       # Makes a more readable output.
-alias df='df -kTh'
-
-alias psu='ps -o pid,rss,command -w'
-
-alias l='ls -alFG'
-
+# Python
 alias mkv='mkvirtualenv --distribute --no-site-packages' 
+
+# Git
 alias gs='git status'
+alias gb='git branch'
 alias gl='git ld --all'
--- misc.sh
+++ misc.sh
@@ -1,5 +1,4 @@
 # Globally exclude some files
-git config --global core.excludesfile ~/etc/bash-ext/.gitignore_global
 git config --global alias.st status
 git config --global alias.last "log -1 HEAD"
 git config --global alias.co checkout
