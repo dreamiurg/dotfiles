76987caa7682998a7dd90b8f52d0ef957e0d0c9e
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-10-01 00:03:51 +0300

    added difftool alias

--- misc.sh
+++ misc.sh
@@ -4,3 +4,4 @@ git config --global alias.last "log -1 HEAD"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.diffall "!sh ~/etc/git-diffall/git-diffall"
