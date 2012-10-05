2878845fa6ee611a8f318af946311a05b8bf2ab6
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2012-10-05 01:13:47 -0700

    colors

--- functions.sh
+++ functions.sh
@@ -4,22 +4,24 @@
 function __define_colors
 {
   # Configure Colors:
-  WHITE='\[\033[1;37m\]'
-  LIGHTGRAY='\[033[0;37m\]'
-  GRAY='\[\033[1;30m\]'
   BLACK='\[\033[0;30m\]'
-  RED='\[\033[0;31m\]'
-  LIGHTRED='\[\033[1;31m\]'
-  GREEN='\[\033[0;32m\]'
-  LIGHTGREEN='\[\033[1;32m\]'
-  BROWN='\[\033[0;33m\]'
-  YELLOW='\[\033[1;33m\]'
   BLUE='\[\033[0;34m\]'
   LIGHTBLUE='\[\033[1;34m\]'
-  PURPLE='\[\033[0;35m\]'
-  PINK='\[\033[1;35m\]'
+  BROWN='\[\033[0;33m\]'
+  LIGHTBROWN='\[\033[1;33m\]'
   CYAN='\[\033[0;36m\]'
   LIGHTCYAN='\[\033[1;36m\]'
+  GRAY='\[\033[1;30m\]'
+  LIGHTGRAY='\[033[0;37m\]'
+  GREEN='\[\033[0;32m\]'
+  LIGHTGREEN='\[\033[1;32m\]'
+  PINK='\[\033[1;35m\]'
+  PURPLE='\[\033[0;35m\]'
+  RED='\[\033[0;31m\]'
+  LIGHTRED='\[\033[1;31m\]'
+  WHITE='\[\033[1;37m\]'
+  YELLOW='\[\033[0;33m\]'
+  LIGHTYELLOW='\[\033[1;33m\]'
   DEFAULT='\[\033[0m\]'
 }
 
@@ -39,13 +41,13 @@ function __venv_ps1 ()
 
 function __set_prompt ()
 {
-  local BASE="${GREEN}\u@\h:${BROWN}\w "
-  local GIT="`EXT_COLOR 25`\$(__git_ps1 '(git:%s) ')"
-#	local VENV="`EXT_COLOR 196`(venv:`basename \"$VIRTUAL_ENV\"`) "
-	local VENV="`EXT_COLOR 196`\$(__venv_ps1 '(venv:%s) ')"
+  local USER="${GREEN}\u@\h"
+  local DIR="${YELLOW}\w"
+	local VENV="${RED}\$(__venv_ps1 '[ve:%s] ')"
+  local GIT="${BLUE}\$(__git_ps1 '[git:%s] ')"
 	local END="${DEFAULT}\\$ "
 	
-  export PS1="${BASE}${VENV}${GIT}${END}"
+  export PS1="${USER}:${DIR} ${VENV}${GIT}${END}"
 	
   # disable prompt modification by default ~/.virtualenv/<envname>/bin/activate
 	export VIRTUAL_ENV_DISABLE_PROMPT=1
