577043ba145a1bd265984af7bc2992b55087d8f8
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2014-01-02 22:24:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 INSTALL_DIR="$HOME/.dotfiles"
-DOTFILES=(vim vimrc screenrc tmux.conf zsh)
+DOTFILES=(vim vimrc screenrc tmux.conf zshrc)
 
 for F in ${DOTFILES[@]}; do
     mv $HOME/.$F $HOME/.$F.bak
