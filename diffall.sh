31ba0efc193350fa67c816ad7698c10d86149610
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2014-07-25 23:08:44 -0700

    Changes

--- diffall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Make `git diffall` to open all files at once. Useful is you use Beyond Compare or other
-# tool that support tabbed interface
-#
-# http://stackoverflow.com/questions/1220309/git-difftool-open-all-diff-files-immediately-not-in-serial
-
-for name in $(git diff --name-only $1); do git difftool $1 $name & done
--- git-setup.sh
+++ git-setup.sh
@@ -11,7 +11,5 @@ git config --global alias.br branch
 git config --global alias.lg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(bold white)— %cn%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
 git config --global alias.lgg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%cD%C(reset) %C(bold green)(%cr)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(bold white)— %cn%C(reset)' --abbrev-commit"
 
-git config --global alias.diffall !"$INSTALL_DIR/git-diffall"
-
 git config --global color.ui always
 git config --global color.diff always
