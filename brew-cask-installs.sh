ca14c9736612b451b9a2649b614b64f707d1b20d
Dmitry Guyvoronsky <the@dreamiurg.net>
2019-12-09 16:08:59 -0800

    Added brew cask installs

---
+++ brew-cask-installs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Absolute basics
+brew cask install dropbox
+brew cask install alfred
+brew cask install evernote
+
+# Mac OS UI essentials
+brew cask install muzzle
+brew cask install hazeover
+
+# IDEs
+brew cask install iterm2
+brew cask install rubymine
+brew cask install pycharm
+brew cask install datagrip
+
