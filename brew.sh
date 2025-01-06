8e430abe9e021b1ef79b98172471a5dc54e545f0
Dmitry Guyvoronsky <the@dreamiurg.net>
2025-01-06 10:35:16 -0800

    Updated git config

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Before basics
+brew install git
+brew install vim
+
+# Absolute basics
+brew install --cask dropbox
+brew install --cask alfred
+brew install --cask google-chrome
+brew install --cask obsidian
+
+# IDEs
+brew install --cask iterm2
+
--- git-setup.sh
+++ git-setup.sh
@@ -11,6 +11,7 @@ git config --global alias.last "log -1 HEAD"
 git config --global alias.co "checkout"
 git config --global alias.br "branch"
 git config --global alias.it '!git init && git commit -m “root” --allow-empty'
+git config --global alias.push "push --force-with-lease"
 git config --global alias.lg "log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(bold white)— %cn%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
 git config --global alias.lga "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%cr)%C(reset) %C(white)%s%C(reset) %C(bold white)— %cn%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
 
